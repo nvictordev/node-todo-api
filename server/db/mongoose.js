3390348ea87911b8d8c2224e6d46714a8c4f3ef4
Victor Nhem <nhemvictor@gmail.com>
2018-07-18 13:58:04 -0500

    Remove unecessary line

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,3 @@ mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
-
-process.env.NODE_ENV === 'test'
-
