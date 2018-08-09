a3984b866282f05f342674355c2e6f4b8e515078
Victor Nhem <nhemvictor@gmail.com>
2018-08-08 22:42:38 -0500

    Add POST /users

---
+++ playground/hashing.js
@@ -0,0 +1,34 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+let data = {
+  id: 10
+};
+
+let token = jwt.sign(data, '123abc');
+console.log(token);
+
+let decoded = jwt.verify(token, '123abc');
+console.log('decoded', decoded);
+
+// let message = 'I am user number 1';
+// let hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`HASH: ${hash}`);
+
+// let data = {
+//   id: 4
+// };
+// let token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// }
+
+// let resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+// if (resultHash === token.hash) {
+//   console.log('Data was not changed');
+// } else {
+//   console.log('Data was tampered')
+// }
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,44 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// {
+//   email: '78sa@gmail.com',
+//   password: 'victor888',
+//   tokens: [{
+//     access: 'auth',
+//     token: 'kfsdljkflsdkjflsk'
+//   }]  
+// }
 
 let User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: (value) => {
+        return validator.isEmail(value);
+      },
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User};
