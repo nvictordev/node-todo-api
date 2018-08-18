d24a87009bc95b8e4400b2650d2ca5450149a1a9
Victor Nhem <nhemvictor@gmail.com>
2018-08-17 21:27:51 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,16 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens: {token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token) {
   let User = this;
 
